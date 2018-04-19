188c192c36af8ca603397684d464150220d554ad
yongsheng5419 <yongsheng5419@163.com>
2018-04-19 21:47:12 +0800

    test01

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("lufei hzw!!随便改改！");
+		System.out.println("lufei hzw!!随便改改！!!");
 	}
 }
